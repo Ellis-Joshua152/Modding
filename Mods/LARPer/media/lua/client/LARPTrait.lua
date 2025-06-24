811d6574b8357ab9c09ffb50dfcfd0bdc5e994c9
Nightkiller0011 <93334340+Nightkiller0011@users.noreply.github.com>
2025-06-24 15:21:51 -0400

    Restructured Folders

---
+++ Mods/LARPer/media/lua/client/LARPTrait.lua
@@ -0,0 +1,10 @@
+-- ProfessionFramework.addTrait('LARPer', {
+--     name = "UI_trait_LARPer",
+--     description = "UI_Trait_LARPerdesc",
+--     icon = "trait_LARPer",
+--     cost = 3,
+--     xp = {
+-- 		[Perks.Tailoring] = 2,
+-- 		[Perks.LongBlunt] = 2,
+--     },
+-- })
