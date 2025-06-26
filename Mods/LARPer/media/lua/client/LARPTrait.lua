ae69f8b12c79a616d22c7246e815133e20427381
Nightkiller0011 <93334340+Nightkiller0011@users.noreply.github.com>
2025-06-26 14:43:26 -0400

    Testing which file needs to contain information

--- Mods/LARPer/media/lua/client/LARPTrait.lua
+++ Mods/LARPer/media/lua/client/LARPTrait.lua
@@ -8,3 +8,11 @@
 -- 		[Perks.LongBlunt] = 2,
 --     },
 -- })
+
+local function LarpTrait()
+    local larp1 = TraitFactory.addTrait("LARPer", getText("UI_Trait_LARPer"), -3, getText("UI_Trait_LARPERdesc"), false, false);
+    larp1:addXPBoost(Perks.Tailoring, 2)
+    larp1:addXPBoost(Perks.LongBlade, 2)
+end
+
+Events.OnGameBoot.add(LarpTrait);
