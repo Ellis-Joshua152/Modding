f9f687b832db6e4d401a52930a41811a7a1334ba
Nightkiller0011 <93334340+Nightkiller0011@users.noreply.github.com>
2025-07-08 15:36:53 -0400

    Finished making changes to mod allowing it to work.

--- Mods/LARPer/media/lua/client/LARPTrait.lua
+++ Mods/LARPer/media/lua/client/LARPTrait.lua
@@ -1,18 +1,11 @@
--- ProfessionFramework.addTrait('LARPer', {
---     name = "UI_trait_LARPer",
---     description = "UI_Trait_LARPerdesc",
---     icon = "trait_LARPer",
---     cost = 3,
---     xp = {
--- 		[Perks.Tailoring] = 2,
--- 		[Perks.LongBlunt] = 2,
---     },
--- })
-
-local function LarpTrait()
-    local larp1 = TraitFactory.addTrait("LARPer", getText("UI_Trait_LARPer"), -3, getText("UI_Trait_LARPERdesc"), false, false);
-    larp1:addXPBoost(Perks.Tailoring, 2)
-    larp1:addXPBoost(Perks.LongBlade, 2)
-end
-
-Events.OnGameBoot.add(LarpTrait);
+ProfessionFramework.addTrait('LARPer', {
+    name = "L.A.R.P.er",
+    description = "Well Aquainted with making costumes and fighting for entertainment.",
+    icon = "trait_LARPer",
+    cost =  3,
+    xp = {
+		[Perks.Tailoring] = 2,
+		[Perks.LongBlade] = 1,
+		[Perks.SmallBlade] = 1,
+    },
+})
--- Mods/LARPer/media/lua/client/LARPTraitTest.lua
+++ Mods/LARPer/media/lua/client/LARPTraitTest.lua
@@ -1,7 +1,9 @@
-local function LarpTrait()
-    local larp1 = TraitFactory.addTrait("LARPer", getText("UI_Trait_LARPer"), -3, getText("UI_Trait_LARPERdesc"), false, false);
-    larp1:addXPBoost(Perks.Tailoring, 2)
-    larp1:addXPBoost(Perks.LongBlade, 2)
-end
 
-Events.OnGameBoot.add(LarpTrait);
+-- local function LarpTrait1()
+--     local larp1 = TraitFactory.addTrait("LARPer", getText("L.A.R.P.er"), -3, getText("Well Aquainted with making costumes and fighting for entertainment."), false, false);
+--     larp1:addXPBoost(Perks.Tailoring, 2)
+--     larp1:addXPBoost(Perks.LongBlade, 1)
+--     larp1:addXPBoost(Perks.SmallBlade, 1)
+-- end
+
+-- Events.OnGameBoot.add(LarpTrait);
