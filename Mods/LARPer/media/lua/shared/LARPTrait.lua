6fce1c7ff500bdd7f2398f10054c0115bf0650db
Nightkiller0011 <93334340+Nightkiller0011@users.noreply.github.com>
2025-07-08 15:40:10 -0400

    Uniformity

--- Mods/LARPer/media/lua/shared/LARPTrait.lua
+++ Mods/LARPer/media/lua/shared/LARPTrait.lua
@@ -1,19 +1,11 @@
 ProfessionFramework.addTrait('LARPer', {
     name = "L.A.R.P.er",
     description = "Well Aquainted with making costumes and fighting for entertainment.",
-    icon = "LARPer",
+    icon = "trait_LARPer",
     cost =  3,
     xp = {
-		[Perks.Tailoring] = 2,
-		[Perks.LongBlade] = 1,
-		[Perks.SmallBlade] = 1,
+		    [Perks.Tailoring] = 2,
+		    [Perks.LongBlade] = 1,
+	    	[Perks.SmallBlade] = 1,
     },
 })
-
--- local function LarpTrait1()
---     local larp1 = TraitFactory.addTrait("LARPer", getText("UI_Trait_LARPer"), -3, getText("UI_Trait_LARPERdesc"), false, false);
---     larp1:addXPBoost(Perks.Tailoring, 2)
---     larp1:addXPBoost(Perks.LongBlade, 2)
--- end
-
--- Events.OnGameBoot.add(LarpTrait);
