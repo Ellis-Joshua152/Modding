24e800a4f21335b846072ed4255eda5127d59bf9
Nightkiller0011 <93334340+Nightkiller0011@users.noreply.github.com>
2025-07-09 17:41:43 -0400

    Created Gamer folder and icon

---
+++ Mods/GameTrait/media/lua/client/GamerTrait.lua
@@ -0,0 +1,14 @@
+ProfessionFramework.addTrait('Gamer', {
+    name = "Gamer",
+    description = "Knows electronics and has 'training' in many forms of combat.",
+    icon = "trait_Gamer",
+    cost =  5,
+    xp = {
+		[Perks.Electrical] = 2,
+		[Perks.Blunt] = 1,
+		[Perks.SmallBlade] = 1,
+		[Perks.Spear] = 1,
+		[Perks.Reloading] = 1,
+		[Perks.Aiming] = 1,
+    },
+})
