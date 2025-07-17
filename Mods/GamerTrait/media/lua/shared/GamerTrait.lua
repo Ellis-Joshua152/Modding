4960708fe747d701af9208bb0c894e31d293bbb3
Nightkiller0011 <93334340+Nightkiller0011@users.noreply.github.com>
2025-07-17 18:28:38 -0400

    Added One Punch man trait

--- Mods/GamerTrait/media/lua/shared/GamerTrait.lua
+++ Mods/GamerTrait/media/lua/shared/GamerTrait.lua
@@ -1,3 +1,22 @@
+ProfessionFramework.addTrait('Gamer', {
+    name = "Gamer",
+    description = "Knows electronics and has 'training' in many forms of combat.",
+    icon = "trait_Gamer",
+    cost = 4,
+    xp = {
+        [Perks.Blunt] = 1,
+        [Perks.Reloading] = 1,
+        [Perks.Aiming] = 1,
+    },
+    OnApply = function(player)
+        local inv = player:getInventory()
+        inv:AddItem("Base.Hammer")
+        inv:AddItem("Base.VideoGame")
+        inv:AddItem("Base.Headphones")
+    end
+})
+
+
 -- require "TraitFactory"
 
 -- local function GamerTrait()
@@ -17,21 +36,3 @@
 -- end
 
 -- Events.OnGameBoot.Add(GamerTrait)
-
-ProfessionFramework.addTrait('Gamer', {
-    name = "Gamer",
-    description = "Knows electronics and has 'training' in many forms of combat.",
-    icon = "trait_Gamer",
-    cost = 4,
-    xp = {
-        [Perks.Blunt] = 1,
-        [Perks.Reloading] = 1,
-        [Perks.Aiming] = 1,
-    },
-    OnApply = function(player)
-        local inv = player:getInventory()
-        inv:AddItem("Base.Hammer")
-        inv:AddItem("Base.VideoGame")
-        inv:AddItem("Base.Headphones")
-    end
-})
