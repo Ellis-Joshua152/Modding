8156b6f7e4a283dd285b2857b2a572378eb2f188
Nightkiller0011 <93334340+Nightkiller0011@users.noreply.github.com>
2025-07-22 13:52:02 -0400

    Cleaned code and deleted duplicates.

--- Mods/GamerTrait/media/lua/shared/GamerTrait.lua
+++ Mods/GamerTrait/media/lua/shared/GamerTrait.lua
@@ -15,24 +15,3 @@ ProfessionFramework.addTrait('Gamer', {
         inv:AddItem("Base.Headphones")
     end
 })
-
-
--- require "TraitFactory"
-
--- local function GamerTrait()
---     local gamer = TraitFactory.addTrait(
---         "Gamer", -- internal ID
---         "Gamer", -- display name
---         4,       -- cost
---         "Knows electronics and has 'training' in many forms of combat.", -- description
---         "trait_Gamer", -- icon name (no .png), matches Trait_Gamer.png in textures
---         false    -- removeOnSandbox (false = trait is always selectable)
---     )
-
---     -- XP Boosts
---     gamer:addXPBoost(Perks.Blunt, 1)
---     gamer:addXPBoost(Perks.Reloading, 1)
---     gamer:addXPBoost(Perks.Aiming, 1)
--- end
-
--- Events.OnGameBoot.Add(GamerTrait)
